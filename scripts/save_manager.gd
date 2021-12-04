e41072795eaa0fff8463e78d2c5baddc507636f2
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-12-04 19:15:59 +0300

    config save system

---
+++ scripts/save_manager.gd
@@ -0,0 +1,28 @@
+extends Node
+
+const config_path = "user://mazedreams.cfg"
+var config_file = ConfigFile.new()
+
+var config_data = {
+	"volume" : {
+		"bgm" : 100.0,
+		"sfx" : 100.0
+	}
+}
+
+func _ready():
+	_load_data()
+
+func _save_data():
+	for section in config_data.keys():
+		for key in config_data[section].keys():
+			config_file.set_value(section, key, config_data[section][key])
+	config_file.save(config_path)
+
+func _load_data():
+	var error = config_file.load(config_path)
+	if error == OK:
+		for section in config_data.keys():
+			for key in config_data[section].keys():
+				var val = config_file.get_value(section,key)
+				config_data[section][key] = val
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,10 +1,20 @@
 extends Control
 
+onready var bgm_slider = $menu/bgm_slider
+onready var sfx_slider = $menu/sfx_slider
+
 func _ready():
+	bgm_slider.value = save_manager.config_data["volume"]["bgm"]
+	sfx_slider.value = save_manager.config_data["volume"]["sfx"]
 	hide()
 
 func _settings_pressed():
 	show()
 
 func _ok_pressed():
+	save_manager.config_data["volume"]["bgm"] = bgm_slider.value
+	AudioServer.set_bus_volume_db(2, linear2db(bgm_slider.value / 100.0))
+	save_manager.config_data["volume"]["sfx"] = sfx_slider.value
+	AudioServer.set_bus_volume_db(1, linear2db(sfx_slider.value / 100.0))
+	save_manager._save_data()
 	hide()
